77675e2c6c4b491ccb375f63b0040b9c50d464b6
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-05-01 15:11:21 -0300

    scss

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import './home.scss'
 
-const Home = () => {
+interface Category {
+  id: number;
+  name: string;
+}
+
+const Home: React.FC = () => {
     console.log("Home")
 
-    const [categories, setCategories] = useState([])
+    const [categories, setCategories] = useState<Category[]>([])
     const [valueInput, setValueInput] = useState<number>(10)
 
     async function getCategories() {
@@ -16,32 +21,35 @@ const Home = () => {
     getCategories().then(data => setCategories(data.trivia_categories))
 
   return (
-    <div className="menu">
-            <h1>Trivia</h1>
+    <div className="menu-container">
+        <div className="menu-wrapper">
+        <h1>Trivia</h1>
             <form className="menu-options">
-                <label htmlFor="categories">Choose a categorie</label>
+                <label htmlFor="categories">Category</label>
                 <select name="categories" id="categories">
-                    {categories.map((category, index) => <option key={index} value={category["id"]}>{category["name"]}</option>)}
+                    {categories.map((category: Category, index: number) => <option key={index} value={category["id"]}>{category["name"]}</option>)}
                 </select>
 
+                <label htmlFor="difficulty">Difficulty</label>
                 <select name="difficulty" id="difficulty">
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>
                 </select>
 
+                <label htmlFor="type">Type</label>
                 <select name="type" id="type">
                     <option value="multiple">Multiple Choice</option>
                     <option value="boolean">True / False</option>
                 </select>
 
-
+                <label htmlFor="amount">Amount</label>
                 <input onChange={(e) => setValueInput(Number(e.target.value))} type="number" name="amount" id="amount" min="1" max="50" value={valueInput}/>
 
-
-                <button>Play</button>
+                <button className='play-btn'>Play</button>
             </form>
-        </div>
+        </div>   
+    </div>
   )
 }
 
